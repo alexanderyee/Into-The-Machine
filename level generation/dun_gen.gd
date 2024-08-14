a83716f31473c058e315ddf714ffda3ca24d8d85
Alex Yee <yeealexi@gmail.com>
2024-08-14 16:04:56 -0700

    add gridmap room generator
    following this tutorial, up to given timestamp https://youtu.be/h64U6j_sFgs?si=ft5sYCKppKjTS3LQ&t=1211

--- level generation/dun_gen.gd
+++ level generation/dun_gen.gd
@@ -8,15 +8,69 @@ func set_start(val:bool) -> void:
 	generate()
 	
 @export var border_size : int = 20 : set = set_border_size
-func set_border_size(val : int)->void:
+func set_border_size(val : int) -> void:
 	border_size = val
 	# we only visualize the border in the editor, not in the game itself
 	if Engine.is_editor_hint():
 		visualize_border()
-	
+
+@export var num_rooms : int = 4
+@export var room_margin : int = 1 # min space between rooms
+@export var room_recursion_limit : int = 15 # max num attempts to generate rooms
+@export var min_room_size : int = 2
+@export var max_room_size : int = 4
+
+var room_tiles : Array[PackedVector3Array] = []
+var room_positions : PackedVector3Array = []
+
 func visualize_border():
-	grid_map.set_cell_item()
+	grid_map.clear()
+	for i in range(-1, border_size + 1):
+		# north edge
+		grid_map.set_cell_item(Vector3i(i, 0, -1), 3)
+		# south edge
+		grid_map.set_cell_item(Vector3i(i, 0, border_size), 3)
+		# east edge
+		grid_map.set_cell_item(Vector3i(border_size, 0, i), 3)
+		# west edge
+		grid_map.set_cell_item(Vector3i(-1, 0, i), 3)
+		
 	
 func generate():
-	print("generating...")
+	room_tiles.clear()
+	room_positions.clear()
+	visualize_border()
+	for i in num_rooms:
+		make_room(room_recursion_limit) # TODO probably a smarter way to do this
+	print(room_positions)
+	
+func make_room(rec : int):
+	if rec <= 0:
+		return
+	
+	var width : int = randi_range(min_room_size, max_room_size)
+	var height : int = randi_range(min_room_size, max_room_size)
+	
+	var start_pos : Vector3i
+	start_pos.x = randi_range(0, border_size - width)
+	start_pos.z = randi_range(0, border_size - height)
+	
+	for row in range(-room_margin, height + room_margin):
+		for col in range(-room_margin, width + room_margin):
+			var pos : Vector3i = start_pos + Vector3i(col, 0, row)
+			if grid_map.get_cell_item(pos) == 0:
+				make_room(rec - 1)
+				return
+			
+	var room : PackedVector3Array = []
+	for row in height:
+		for col in width:
+			var pos : Vector3i = start_pos + Vector3i(col, 0, row)
+			grid_map.set_cell_item(pos, 0)
+			room.append(pos)
+	room_tiles.append(room)
+	var avg_x : float = start_pos.x + (float(width) / 2)
+	var avg_z : float = start_pos.z + (float(height) / 2)
+	var pos : Vector3 = Vector3(avg_x, 0, avg_z)
+	room_positions.append(pos)
 	
