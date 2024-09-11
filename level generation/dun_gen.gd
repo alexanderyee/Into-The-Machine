7f7ad26d75667e6b642793f715ebc057704f6cda
Alex Yee <yeealexi@gmail.com>
2024-09-11 16:22:49 -0700

    testing stuff

--- level generation/dun_gen.gd
+++ level generation/dun_gen.gd
@@ -6,7 +6,8 @@ extends Node3D
 @export var start : bool = false : set = set_start
 func set_start(val:bool) -> void:
 	generate()
-
+func _ready():
+	generate()
 # border size is the size of the outermost layer	
 @export var border_size : int = 20 : set = set_border_size
 func set_border_size(val : int) -> void:
@@ -14,6 +15,8 @@ func set_border_size(val : int) -> void:
 	# we only visualize the border in the editor, not in the game itself
 	if Engine.is_editor_hint():
 		visualize_border()
+		visualize_spawn_room()
+		visualize_doors()
 
 @export var num_layers : int = 4
 @export var layer_margin : int = 4
@@ -21,8 +24,9 @@ func set_border_size(val : int) -> void:
 @export var max_room_size : int = 4
 @export var room_margin : int = 4 # min space between secret rooms
 @export var spawn_position : Vector3i = Vector3i(-1, 0, 10)
-var room_tiles : Array[PackedVector3Array] = []
-var room_positions : PackedVector3Array = []
+@export var shape : StringName = StringName("")
+
+enum TILES {ROOM, HALLWAY, DOOR, BORDER, SPAWN}
 
 
 func visualize_border():
@@ -30,47 +34,56 @@ func visualize_border():
 	for layer in range(num_layers):
 		var layer_padding = layer * layer_margin
 		for i in range(-1 + layer_padding, border_size + 1 - layer_padding):
-			#TODO: enums for gridmap cell items?
 			# north edge
-			grid_map.set_cell_item(Vector3i(i , 0, -1 + layer_padding), 0)
+			grid_map.set_cell_item(Vector3i(i , 0, -1 + layer_padding), TILES.ROOM)
 			# south edge
-			grid_map.set_cell_item(Vector3i(i, 0, border_size - layer_padding), 0)
+			grid_map.set_cell_item(Vector3i(i, 0, border_size - layer_padding), TILES.ROOM)
 			# east edge
-			grid_map.set_cell_item(Vector3i(border_size - layer_padding, 0, i), 0)
+			grid_map.set_cell_item(Vector3i(border_size - layer_padding, 0, i), TILES.ROOM)
 			# west edge
-			grid_map.set_cell_item(Vector3i(-1 + layer_padding, 0, i), 0)
-	grid_map.set_cell_item(spawn_position, 4)
+			grid_map.set_cell_item(Vector3i(-1 + layer_padding, 0, i), TILES.ROOM)
+
+func visualize_spawn_room():
+	grid_map.set_cell_item(spawn_position, TILES.SPAWN)
+
+func visualize_doors():
+	# door to next layer can't be within view from previous door or spawn
+	print(shape)
+	if shape == &"Square":
+		# if it's a square, we just make sure that the door isn't on the same
+		# x or z axis
+		var current_position = spawn_position
+		var layer_tiles : PackedVector3Array = get_square_room_tiles(current_position)
+		print(layer_tiles)
+	pass
 	
+# gets all room tiles within the same layer as position	
+func get_square_room_tiles(position : Vector3i):
+	var room_tiles : PackedVector3Array = [position]
+	
+	var current_position = position
+	var directions = [Vector3i.FORWARD, Vector3i.RIGHT,
+		Vector3i.BACK, Vector3i.LEFT]
+	var direction_index = 0
+	for i in range(directions.size()):
+		if grid_map.get_cell_item(current_position + directions[i]) == TILES.ROOM:
+			direction_index = i
+			current_position = current_position + directions[i]
+			room_tiles.append(current_position)
+			break
+	var next_position = current_position + directions[direction_index]
+	while next_position != position:
+		while grid_map.get_cell_item(next_position) == TILES.ROOM:
+			room_tiles.append(next_position)
+			current_position = next_position
+			next_position = current_position + directions[direction_index]
+		direction_index = (direction_index + 1) % directions.size() 
+		next_position = current_position + directions[direction_index]
+
 func generate():
 	visualize_border()
-	
-func make_room(rec : int):
-	if rec <= 0:
-		return
-	
-	var width : int = randi_range(min_room_size, max_room_size)
-	var height : int = randi_range(min_room_size, max_room_size)
-	
-	var start_pos : Vector3i
-	start_pos.x = randi_range(0, border_size - width)
-	start_pos.z = randi_range(0, border_size - height)
-	
-	for row in range(-room_margin, height + room_margin):
-		for col in range(-room_margin, width + room_margin):
-			var pos : Vector3i = start_pos + Vector3i(col, 0, row)
-			if grid_map.get_cell_item(pos) == 0:
-				make_room(rec - 1)
-				return
-			
-	var room : PackedVector3Array = []
-	for row in height:
-		for col in width:
-			var pos : Vector3i = start_pos + Vector3i(col, 0, row)
-			grid_map.set_cell_item(pos, 0)
-			room.append(pos)
-	room_tiles.append(room)
-	var avg_x : float = start_pos.x + (float(width) / 2)
-	var avg_z : float = start_pos.z + (float(height) / 2)
-	var pos : Vector3 = Vector3(avg_x, 0, avg_z)
-	room_positions.append(pos)
+	visualize_spawn_room()
+	visualize_doors()
+
+
 	
